62940e88e83dd91796676a57e59532854007bedf
Jojola07 <jojola10@proton.me>
2026-04-01 15:59:14 -0300

    Gerenciador de Senhas (v1)

---
+++ main.py
@@ -0,0 +1,16 @@
+# PROJETO DE CRIAR UM GERENCIADOR DE SENHAS
+
+import time
+
+while True:
+    print("== GERENCIADOR DE SENHAS ==")
+    time.sleep(1)
+    print(" - MENU DE OPÇÕES -")
+    time.sleep(1)
+    print("1. Adicionar nova senha")
+    print("2. Listar senhas")
+    print("3. Buscar senha")
+    print("4. Excluir senha")
+    print("5. Sair")
+    time.sleep(1)
+    opcao = input("Escolha uma opção: ")

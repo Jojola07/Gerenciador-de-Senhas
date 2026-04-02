e5a2464954c44679155b2f595d7d0c244301dba8
Jojola07 <jojola10@proton.me>
2026-04-02 17:31:58 -0300

    v8 mudei o nome da variavel do json, fiz a diferenciacao de maiusculas e minusculas, identacao nos nomes

--- main.py
+++ main.py
@@ -2,18 +2,18 @@
 
 import time,json
 
-senhas = "senhas.json"
+senhas_file = "senhas.json"
 
 # Carregar senhas do arquivo JSON, se existir
 try:
-    with open(senhas, "r") as f:
+    with open(senhas_file, "r") as f:
         senhas = json.load(f)
 except FileNotFoundError:
     senhas = {}
 
 def salvar_senhas():
-    with open("senhas.json", "w") as f:
-        json.dump(senhas, f)   
+    with open(senhas_file, "w") as f:
+        json.dump(senhas, f, indent=4)   
 while True:
     print("== GERENCIADOR DE SENHAS ==")
     time.sleep(1)
@@ -27,7 +27,15 @@ while True:
     time.sleep(1)
     opcao = input("Escolha uma opção: ")
     if opcao == "1": # ADICIONAR NOVA SENHA
-        nome = input("Digite o nome do serviço: ")
+        nome = input("Digite o nome do serviço: ").lower()
+        if nome in senhas:
+            substituir = input("Esse serviço já está no sistema, deseja substituir a senha? (s/n): ")
+            if substituir.lower() != 's':
+                print("Operação cancelada.")
+                time.sleep(1)
+                print("Voltando ao Menu...")
+                time.sleep(2)
+                continue
         senha = input("Digite a senha: ")
         senhas[nome] = senha
         salvar_senhas()
@@ -37,22 +45,22 @@ while True:
         if senhas:
             for nome, senha in senhas.items():
 
-                print(f"Serviço: {nome} | Senha: {senha}")
+                print(f"Serviço: {nome.title()} | Senha: *******")
         else:
             print("Nenhuma senha cadastrada.")
             time.sleep(1)
             continue
     elif opcao == "3":  # PROCURAR SENHAS
-        nome = input("Digite o nome do serviço para buscar a senha: ")
+        nome = input("Digite o nome do serviço para buscar a senha: ").lower()
         if nome in senhas:
-            print(f"A senha para {nome} é: {senhas[nome]}")
+            print(f"A senha para {nome.title()} é: {senhas[nome]}")
         else:
             print("Serviço não encontrado.")
             time.sleep(1)
             print("Voltando ao Menu...")
             time.sleep(2)
     elif opcao == "4": # EXCLUIR SENHA
-        nome = input("Digite o nome do serviço para excluir a senha: ")
+        nome = input("Digite o nome do serviço para excluir a senha: ").lower()
         if nome in senhas:
             deletarConfirmacao = input(f"Tem certeza que deseja excluir a senha para {nome}? (s/n): ")
             if deletarConfirmacao.lower() != 's':
